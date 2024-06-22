e605cd9c9723fde4f6c3cb1833ef57f9281583b2
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-06-22 11:21:21 +0530

    comments added

--- main.js
+++ main.js
@@ -1,43 +1,73 @@
-const {app,BrowserWindow,ipcMain, dialog} = require('electron');
-const path = require('path');
-const isMac = process.platform === 'darwin';
-const  {testWebSocket} = require('./app');
-// const notifier = require('node-notifier');
-async function handleFileOpen () {
-    // notifier.notify({
-    //     appName: "com.NlvlTracker.id", 
-    //     title: 'Nlvl Tracker',
-    //     message: 'Data Collection Started'
-    //     });
-    testWebSocket()
-        // main()
-    }
-
-function Boot(){
-    const mainWindow = new BrowserWindow({
-        title:'Nlvl Tracker',
-        width:600,
-        height:500,
-        webPreferences: {
-            preload: path.join(__dirname, 'paload.js')
-        }
-    });
-    mainWindow.loadFile(path.join(__dirname,'./views/index.html'));
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const path = require("path");
+const isMac = process.platform === "darwin";
+const { App } = require("./index");
+/**
+ * This function handles the opening of a file.
+ * It is an asynchronous function that calls the App function.
+ *
+ * @async
+ * @function handleFileOpen
+ * @returns {void}
+ */
+async function handleFileOpen() {
+  App();
 }
 
+/**
+ * This function initializes the main window of the application.
+ * It creates a new BrowserWindow with specified options and loads the index.html file.
+ *
+ * @function Boot
+ * @returns {void}
+ */
+function Boot() {
+  const mainWindow = new BrowserWindow({
+    title: "Nlvl Tracker", // The title displayed in the title bar of the window.
+    width: 600, // The width of the window in pixels.
+    height: 500, // The height of the window in pixels.
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"), // The path to the preload script.
+    },
+  });
+
+  // Load the index.html file into the main window.
+  mainWindow.loadFile(path.join(__dirname, "./views/index.html"));
+}
+
+/**
+ * This function is a callback function that is executed when the application is ready.
+ * It sets up the IPC communication for handling file open dialogs and initializes the main window.
+ *
+ * @function
+ * @returns {void}
+ */
 app.whenReady().then(() => {
-    ipcMain.handle('dialog:openFile', handleFileOpen)
-    Boot();
-    app.on('activate',()=>{
-        if (BrowserWindow.getAllWindows().length ===0) {
-            Boot()
-        }
-    })
+  // Register an IPC handler for the 'dialog:openFile' event.
+  // When this event is received, the 'handleFileOpen' function will be called.
+  ipcMain.handle("dialog:openFile", handleFileOpen);
+
+  Boot(); // Initialize the main window of the application.
+
+  // Listen for the 'activate' event.
+  // This event is emitted when the application is activated
+  app.on("activate", () => {
+    // If no windows are open, initialize a new main window.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      Boot();
+    }
+  });
 });
 
-
-app.on('window-all-closed',() =>{
-    if (!isMac) {
-        app.quit()
-    }
-})
+/**
+ * This function handles the closing of all windows when the application is quit.
+ * It checks if the operating system is not macOS and then quits the application.
+ *
+ * @function closeAllWindows
+ * @returns {void}
+ */
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
