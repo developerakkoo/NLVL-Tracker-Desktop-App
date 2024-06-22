d9eb66c91eccff599a0e5b20a176ed5e064f0e3a
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-06-22 11:26:23 +0530

    code resturcure and comments added

---
+++ index.js
@@ -0,0 +1,49 @@
+const { mainFunctionTimer } = require("./constant");
+const { fetchYahooFinanceData } = require("./app");
+const { covert1 } = require("./utils/dataConvertor.util");
+const myInterval = setInterval(App, mainFunctionTimer);
+
+/**
+ * The main function that is called within an interval set in the mainFunctionTimer.
+ * It logs a message to the console and calls the writeToSheet function.
+ *
+ * @returns {void}
+ */
+function App() {
+  console.log("main");
+  writeToSheet();
+}
+
+/**
+ * This function fetches data from Yahoo Finance API, processes it, and writes it to a sheet.
+ * It is called within an interval set in the main function.
+ *
+ * @returns {Promise<void>}
+ */
+async function writeToSheet() {
+  // Fetch data from Yahoo Finance API
+  let DATA = await fetchYahooFinanceData();
+
+  // Check if there was an error fetching data
+  if (DATA.error) {
+    console.error("Error:", DATA.error);
+    return;
+  }
+
+  try {
+    console.log("18: covert Data For Sheet1");
+
+    // Convert the fetched data to the required format for Sheet 1
+    covert1(DATA, 0);
+
+    // Update Sheet 1 with the converted data
+    // ...
+  } catch (error) {
+    console.log(
+      "23: Internet Speed Is SLOW Or Server Not Responding",
+      error.message
+    );
+  }
+}
+
+module.exports = { App };
--- paload.js
+++ paload.js
@@ -1,5 +1,29 @@
-const { contextBridge, ipcRenderer } = require('electron')
+/**
+ * Exposes Electron's IPCRenderer and contextBridge to the main world.
+ * This allows communication between the renderer process (web page) and the main process.
+ *
+ * @module electronAPI
+ */
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  openFile: () => ipcRenderer.invoke('dialog:openFile')
-})
+const { contextBridge, ipcRenderer } = require("electron");
+
+/**
+ * Exposes a function to open a file dialog in the main process.
+ *
+ * @function openFile
+ * @returns {Promise<string[]>} - A promise that resolves to an array of file paths selected by the user.
+ *
+ * @example
+ * // In the renderer process
+ * const { electronAPI } = window;
+ * electronAPI.openFile()
+ *   .then(filePaths => {
+ *     console.log('Selected files:', filePaths);
+ *   })
+ *   .catch(err => {
+ *     console.error('Error opening file:', err);
+ *   });
+ */
+contextBridge.exposeInMainWorld("electronAPI", {
+  openFile: () => ipcRenderer.invoke("dialog:openFile"),
+});
